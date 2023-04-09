5c679b7fc0de8964f1a307e29b61641234f2d57b
user <aytem95@gmail.com>
2023-04-10 02:48:13 +0600

    import add_numbers, sub_numbers

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from funcs import add_numbers
+from funcs import add_numbers, sub_numbers
 
-result = add_numbers(2, 3)
-print(result)
+result1 = add_numbers(2, 3)
+print(result1)
+
+result2 = sub_numbers(2, 3)
+print(result2)
+
